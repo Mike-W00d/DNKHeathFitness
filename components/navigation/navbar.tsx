43b7297ea4bb6dd3e7ae3483eed4a0b626cf2570
MichaelWood <mgwood22@sky.com>
2025-01-22 15:06:36 +0000

    Zenfit added to Nav, needs link adding

--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -7,6 +7,7 @@ import MobileNav from './mobile-nav'
 const Navbar = () => {
   return (
     <nav className="flex flex-row justify-between z-50 w-full gap-5 p-6 sm:px-12 items-center">
+      {/* Logo */}
       <Link href="/" className='flex flex-row items-center gap-2'>
         <Image 
           src="/logo.jpg" 
@@ -15,19 +16,24 @@ const Navbar = () => {
           height={60} 
           className='rounded-md'
         />
+        {/* Site Title */}
         <h1 className='text-4xl font-bold tracking-widest'> 
           DNK Health + Fitness 
         </h1>
       </Link>
+      {/* Navigation Links */}
       <div className='gap-5 flex flex-row place-content-end max-sm:hidden'> 
         <Link href='/reviews'>
-          <button className="yellow-btn sm:h-auto md:h-[52px]">Customer Testimonials</button>
+          <button className="yellow-btn min-w-20 sm:h-auto md:h-[52px]">Customer Testimonials</button>
         </Link>
+        {/* add Link in here once got */}
+          <button className="yellow-btn min-w-20 sm:h-auto md:h-[52px]">Zenfit</button>
         <Link href="/booking">
-          <button className="yellow-btn sm:h-auto md:h-[52px]">Book A Session</button>
+          <button className="yellow-btn min-w-20 sm:h-auto md:h-[52px]">Book A Session</button>
         </Link>
       </div>
       <div className='sm:hidden'> 
+        {/* Mobile Nav imported from seperate */}
         <MobileNav />
       </div>
     </nav>  
